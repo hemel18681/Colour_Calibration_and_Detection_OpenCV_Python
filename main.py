d4f079ba39e33f9927b11687a6d06ee78823e9a4
hemel18681 <hemel18103112@gmail.com>
2020-10-12 00:29:56 +0600

    work more

--- main.py
+++ main.py
@@ -57,10 +57,29 @@ def colorCalibrationMethod():
     while (1):
 
         _, img = cam.read()
+        text = "Normal | Press P to take image."
         img = imutils.resize(img, width=500)
+        grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gaussianImg = cv2.GaussianBlur(grayImg, (21, 21), 0)
+        if firstFrame is None:
+            firstFrame = gaussianImg
+            continue
+        imgDiff = cv2.absdiff(firstFrame, grayImg)
+        threshImg = cv2.threshold(imgDiff, 25, 255, cv2.THRESH_BINARY)[1]
+        threshImg = cv2.dilate(threshImg, None, iterations=2)
+        cnts = cv2.findContours(threshImg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        for c in cnts:
+            if cv2.contourArea(c) < area:
+                continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = "Moving Object Detected."
+        print(text)
+        cv2.putText(img, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         cv2.imshow("my_cam", img)
         key = cv2.waitKey(1) & 0xFF
-        if key == ord("p"):
+        if text == "Moving Object Detected." and key == ord("p"):
             cv2.imwrite("screenshot.jpg", img)
             ok = True
 
@@ -108,6 +127,7 @@ def colorCalibrationMethod():
                         (textColour, textColour, textColour), 2)
             cv2.putText(res, 'Low HSV: ' + str(lowhsv), (5, 240 + height), textFont, textScale,
                         (textColour, textColour, textColour), 2)
+
             # Show image with only specified color range
             cv2.imshow('Found Colours', res)
             # Adjust window sizes
