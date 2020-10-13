2c6c10e92470693894bc9e4282288a21bcc044bb
hemel18681 <hemel18103112@gmail.com>
2020-10-13 13:37:55 +0600

    work done

--- main.py
+++ main.py
@@ -1,145 +1,90 @@
-
+# finding hsv range of target object(pen)
+import cv2
+import numpy as np
 import time
 
-import imutils
-import numpy as np
-import cv2
-from mss import mss
-from PIL import Image
+
+# A required callback method that goes into the trackbar function.
+def nothing(x):
+    pass
 
 
+# Initializing the webcam feed.
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
 
-def colorCalibrationMethod():
-    ok = False
-    cam = cv2.VideoCapture(0)
-    time.sleep(1)
-    firstFrame = None
-    area = 500
+# Create a window named trackbars.
+cv2.namedWindow("Trackbars")
 
-    # Trackbar onChange
-    def nothing(x):
-        pass
+# Now create 6 trackbars that will control the lower and upper range of
+# H,S and V channels. The Arguments are like this: Name of trackbar,
+# window name, range,callback function. For Hue the range is 0-179 and
+# for S,V its 0-255.
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
 
-    # Create new windows
-    colour = np.zeros((50, 300, 3), np.uint8)
-    cv2.namedWindow('Colour Adjustment')
-    cv2.namedWindow('Capture Area')
-    cv2.imshow('Colour Adjustment', colour)
+while True:
 
-    # Create trackbars
-    cv2.createTrackbar('X', 'Capture Area', 0, 1000, nothing)
-    cv2.createTrackbar('Y', 'Capture Area', 0, 1000, nothing)
-    cv2.createTrackbar('Width', 'Capture Area', 0, 500, nothing)
-    cv2.createTrackbar('Height', 'Capture Area', 0, 500, nothing)
-    cv2.createTrackbar('R High', 'Colour Adjustment', 255, 255, nothing)
-    cv2.createTrackbar('G High', 'Colour Adjustment', 255, 255, nothing)
-    cv2.createTrackbar('B High', 'Colour Adjustment', 255, 255, nothing)
-    cv2.createTrackbar('R Low', 'Colour Adjustment', 0, 255, nothing)
-    cv2.createTrackbar('G Low', 'Colour Adjustment', 0, 255, nothing)
-    cv2.createTrackbar('B Low', 'Colour Adjustment', 0, 255, nothing)
+    # Start reading the webcam feed frame by frame.
+    ret, frame = cap.read()
+    if not ret:
+        break
+    # Flip the frame horizontally (Not required)
+    frame = cv2.flip(frame, 1)
 
-    # Create memory of trackbar values to register future changes
-    or1 = cv2.getTrackbarPos('R High', 'Colour Adjustment')
-    og1 = cv2.getTrackbarPos('G High', 'Colour Adjustment')
-    ob1 = cv2.getTrackbarPos('B High', 'Colour Adjustment')
-    or2 = cv2.getTrackbarPos('R Low', 'Colour Adjustment')
-    og2 = cv2.getTrackbarPos('G Low', 'Colour Adjustment')
-    ob2 = cv2.getTrackbarPos('B Low', 'Colour Adjustment')
+    # Convert the BGR image to HSV image.
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Initialize variables
-    textColour = 255
-    textScale = 0.7
-    textFont = cv2.FONT_HERSHEY_SIMPLEX
-    highhsv = [0, 0, 0]
-    lowhsv = [0, 0, 0]
-    sct = mss()
+    # Get the new values of the trackbar in real time as the user changes
+    # them
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
 
-    while (1):
+    # Set the lower and upper HSV range according to the value selected
+    # by the trackbar
+    lower_range = np.array([l_h, l_s, l_v])
+    upper_range = np.array([u_h, u_s, u_v])
 
-        _, img = cam.read()
-        text = "Normal | Press P to take image."
-        img = imutils.resize(img, width=500)
-        grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gaussianImg = cv2.GaussianBlur(grayImg, (21, 21), 0)
-        if firstFrame is None:
-            firstFrame = gaussianImg
-            continue
-        imgDiff = cv2.absdiff(firstFrame, grayImg)
-        threshImg = cv2.threshold(imgDiff, 25, 255, cv2.THRESH_BINARY)[1]
-        threshImg = cv2.dilate(threshImg, None, iterations=2)
-        cnts = cv2.findContours(threshImg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        for c in cnts:
-            if cv2.contourArea(c) < area:
-                continue
-            (x, y, w, h) = cv2.boundingRect(c)
-            #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Moving Object Detected."
-        print(text)
-        cv2.putText(img, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.imshow("my_cam", img)
-        key = cv2.waitKey(1) & 0xFF
-        if text == "Moving Object Detected." and key == ord("p"):
-            cv2.imwrite("screenshot.jpg", img)
-            ok = True
+    # Filter the image and get the binary mask, where white represents
+    # your target color
+    mask = cv2.inRange(hsv, lower_range, upper_range)
 
-        if ok:
-            # Get current HSV values
-            r1 = cv2.getTrackbarPos('R High', 'Colour Adjustment')
-            g1 = cv2.getTrackbarPos('G High', 'Colour Adjustment')
-            b1 = cv2.getTrackbarPos('B High', 'Colour Adjustment')
-            r2 = cv2.getTrackbarPos('R Low', 'Colour Adjustment')
-            g2 = cv2.getTrackbarPos('G Low', 'Colour Adjustment')
-            b2 = cv2.getTrackbarPos('B Low', 'Colour Adjustment')
-            # Get current capture area
-            x = cv2.getTrackbarPos('X', 'Capture Area')
-            y = cv2.getTrackbarPos('Y', 'Capture Area')
-            width = cv2.getTrackbarPos('Width', 'Capture Area')
-            height = cv2.getTrackbarPos('Height', 'Capture Area')
-            # Change color bar on changing trackbar (high or low)
-            if or1 != r1 or og1 != g1 or ob1 != b1:
-                or1 = r1
-                og1 = g1
-                ob1 = b1
-                colour[:] = [b1, g1, r1]
-                highhsv = cv2.cvtColor(colour, cv2.COLOR_BGR2HSV)
-                highhsv = [int(highhsv[0][0][0]), int(highhsv[0][0][1]), int(highhsv[0][0][2])]
-            elif or2 != r2 or og2 != g2 or ob2 != b2:
-                or2 = r2
-                og2 = g2
-                ob2 = b2
-                colour[:] = [b2, g2, r2]
-                lowhsv = cv2.cvtColor(colour, cv2.COLOR_BGR2HSV)
-                lowhsv = [int(lowhsv[0][0][0]), int(lowhsv[0][0][1]), int(lowhsv[0][0][2])]
-            cv2.imshow('Colour Adjustment', colour)
-            # Capture screen
-            imge = Image.open(r"screenshot.jpg")
-            img_np = np.array(imge)
-            # Convert and find colours from captured images
-            hsv = cv2.cvtColor(img_np, cv2.COLOR_BGR2HSV)
-            rgb = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-            higher = np.array(highhsv)
-            lower = np.array(lowhsv)
-            mask = cv2.inRange(hsv, lower, higher)
-            res = cv2.bitwise_and(rgb, rgb, mask=mask)
-            # Display HSV over captured image
-            cv2.putText(res, 'High HSV: ' + str(highhsv), (5, 210 + height), textFont, textScale,
-                        (textColour, textColour, textColour), 2)
-            cv2.putText(res, 'Low HSV: ' + str(lowhsv), (5, 240 + height), textFont, textScale,
-                        (textColour, textColour, textColour), 2)
+    # You can also visualize the real part of the target color (Optional)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
 
-            # Show image with only specified color range
-            cv2.imshow('Found Colours', res)
-            # Adjust window sizes
-            cv2.resizeWindow('Colour Adjustment', 400, 50)
-            cv2.resizeWindow('Capture Area', 300, 0)
-            # Break loop with esc key
-            k = cv2.waitKey(1) & 0xFF
-            if k == 27:
-                break
-    cv2.destroyAllWindows()
+    # Converting the binary mask to 3 channel image, this is just so
+    # we can stack it with the others
+    mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
+    # stack the mask, orginal frame and the filtered result
+    stacked = np.hstack((mask_3, frame, res))
 
+    # Show this stacked frame at 40% of the size.
+    cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.4, fy=0.4))
 
-#saveImage()
-colorCalibrationMethod()
+    # If the user presses ESC then exit the program
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+    # If the user presses `s` then print this array.
+    if key == ord('s'):
+        thearray = [[l_h, l_s, l_v], [u_h, u_s, u_v]]
+        print(thearray)
+
+        # Also save this array as penval.npy
+        np.save('hsv_value', thearray)
+        break
+
+# Release the camera & destroy the windows.
+cap.release()
+cv2.destroyAllWindows()
